182539db301ac0e8afaf16d5ea1c1064e04df75f
leosilka <leonardosilka@hotmail.com>
2022-12-08 00:10:01 -0300

    Alterações feitas via VS Code

--- projeto.py
+++ projeto.py
@@ -11,25 +11,30 @@ while escolha != 5:
     [ 2 ] - Jogo de Jokenpô
     [ 5 ] - Encerrar Menu''')
     escolha = int(input('Digite a sua escolha: '))
-    if escolha == 1: #Aqui está o programa de Adivinhação
+    if escolha == 1:  # Aqui está o programa de Adivinhação
         cont = 0
         erros = 0
         acertos = 0
         while r == 'S':
             cont += 1
             nsorteado = random.randint(1, 10)
-            nescolhido = int(input('Tente adivinhar o número que estou pensando entre 0 e 10: '))
+            nescolhido = int(
+                input('Tente adivinhar o número que estou pensando entre 0 e 10: '))
             if nescolhido == nsorteado:
                 print('Parabéns, você acertou o número que estava pensando.')
                 acertos += 1
-                r = str(input('Deseja continuar jogando? [S/N]')).strip().upper()
+                r = str(
+                    input('Deseja continuar jogando? [S/N]')).strip().upper()
             elif nescolhido != nsorteado:
-                print('Infelizmente você errou o número qeu estava pensando. O número que pensei foi {}.' .format(nsorteado))
+                print('Infelizmente você errou o número qeu estava pensando. O número que pensei foi {}.' .format(
+                    nsorteado))
                 erros += 1
-                r = str(input('Deseja continuar jogando? [S/N]')).strip().upper()
-        print('Você jogou {} vezes, teve {} acertos e {} erros.' .format(cont, acertos, erros))
+                r = str(
+                    input('Deseja continuar jogando? [S/N]')).strip().upper()
+        print('Você jogou {} vezes, teve {} acertos e {} erros.' .format(
+            cont, acertos, erros))
         print('Retornando ao menu...')
-    elif escolha == 2: #Aqui está o programa de Jokenpô
+    elif escolha == 2:  # Aqui está o programa de Jokenpô
         cont = 0
         erros = 0
         acertos = 0
@@ -47,46 +52,50 @@ while escolha != 5:
             print('Computador jogou {}' .format(jokenpo[ia]))
             print('Jogador jogou {}' .format(jokenpo[player]))
             print('-'*20)
-            if ia == 0: #IA jogou Pedra
+            if ia == 0:  # IA jogou Pedra
                 if player == 0:
                     print('Empatamos!')
                     empates += 1
                 elif player == 1:
                     print('Você Ganhou!')
-                    acertos +=1
+                    acertos += 1
                 elif player == 2:
                     print('Você Perdeu!')
                     erros += 1
                 else:
                     print('Jogada Inválida!')
-                r = str(input('Deseja continuar jogando? [S/N]')).strip().upper()
-            elif ia == 1: #IA jogou Papel
+                r = str(
+                    input('Deseja continuar jogando? [S/N]')).strip().upper()
+            elif ia == 1:  # IA jogou Papel
                 if player == 0:
                     print('Você Perdeu!')
                     erros += 1
                 elif player == 1:
                     print('Empatamos')
-                    empates +=1
+                    empates += 1
                 elif player == 2:
                     print('Você Ganhou!')
                     acertos += 1
                 else:
                     print('Jogada Inválida!')
-                r = str(input('Deseja continuar jogando? [S/N]')).strip().upper()
-            elif ia == 2: #IA jogou Tesoura
+                r = str(
+                    input('Deseja continuar jogando? [S/N]')).strip().upper()
+            elif ia == 2:  # IA jogou Tesoura
                 if player == 0:
                     print('Você Ganhou!')
                     acertos += 1
                 elif player == 1:
                     print('Você Perde!')
-                    erros +=1
+                    erros += 1
                 elif player == 2:
                     print('Empatamos!')
                     empates += 1
                 else:
                     print('Jogada Inválida!')
-                r = str(input('Deseja continuar jogando? [S/N]')).strip().upper()
-        print('Você teve {} acertos, {} empates e {} derrotas.' .format(acertos, empates, erros))
+                r = str(
+                    input('Deseja continuar jogando? [S/N]')).strip().upper()
+        print('Você teve {} acertos, {} empates e {} derrotas.' .format(
+            acertos, empates, erros))
         print('Retornando ao menu...')
         sleep(3)
     elif escolha == 5:
